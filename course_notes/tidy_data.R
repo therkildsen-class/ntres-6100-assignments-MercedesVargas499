e9d1dc8d38adefe28a21352065d81b877f4f2d1d
MercedesVargas499 <mv499@cornell.edu>
2025-09-25 11:20:54 -0400

    tidy

--- course_notes/tidy_data.R
+++ course_notes/tidy_data.R
@@ -33,5 +33,53 @@ table2 |>
 ##el punto era mostrar que la tabla mal ordenada no me deja trabajar
 
 
+##para ordenar las tablas tenemos que modificar las columnas y darles nombres
+table4a |> 
+  pivot_longer(
+    cols = c("1999", "2000"),
+    names_to = "year",
+    values_to = "cases"
+  )
+
+##el cols = puede estar o no, funciona igual de ambas maneras
+table4b |> 
+  pivot_longer(
+    c("1999","2000"),  ## la función c() (“combine”) crea un vector.
+    names_to = "year",
+    values_to = "population"
+  )
+
+##es importante saber que esto ordena, pero no guarda los datos
+##si la quiero guardar (pero no es permanente, lo vemos la clase que viene):
+table4b_tidy<- table4b |> 
+  pivot_longer(
+    c("1999","2000"),
+    names_to = "year",
+    values_to = "population"
+  )
+
+table2 |>  ##aca queremos que a cada linea le corresponda una observacion (cases y population a columnas)
+pivot_wider(names_from = type,
+            values_from = count)
 
 
+##pivot_longer() → “apila” columnas → más filas, menos columnas.
+
+##pivot_wider() → “desapila” filas → más columnas, menos filas.
+
+table3 |>  ##en esa tabla tiene dos datos por columna (casos y poblacion incluidos en rate)
+     separate(rate, ##que separamos?
+              into = c("cases","population"),  ##en que lo convertimos??
+              sep="/")  ##aca le decimos donde tiene que separar
+
+
+table5<- table3 |> 
+  separate(year,
+           into= c("century", "year"),
+           sep=2)
+
+table5 |> 
+  unite(fullyear,
+        century,
+        year,
+        sep="")
